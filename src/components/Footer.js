7a7c2126f61941250a8a434733d865eacdbb05eb
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2020-09-06 15:43:19 -0700

    added footer and cleaned up full page sizing, responsive styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,28 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__left-container">
         <p className="footer__text">
-          Application by
-          <br />
+          Application by <br className="footer__break" />
           <span className="footer__text--emph">Jess Hilliam</span>
           <br />
           for Shopify Internship Winter 2021
         </p>
+        <div className="footer__contact-links">
+          <a
+            href="https://github.com/pupocalypse"
+            className="footer__link"
+            target="_blank"
+          >
+            <p className="footer__text">GitHub</p>
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://www.linkedin.com/in/jess-hilliam/"
+            className="footer__link"
+            target="_blank"
+          >
+            <p className="footer__text">LinkedIn</p>
+          </a>
+        </div>
       </div>
       <div className="footer__right-container">
         <p className="footer__text">
