cd31693956cca97141f7f4189efea38e04204100
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2020-09-07 16:11:08 -0700

    Updates

---
+++ precache-manifest.71b50d8b6b1386f6e2a323747d2efee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0f13c7f134c33ed8b663fdb0699ef0",
+    "url": "/shopify_challenge_shoppies/index.html"
+  },
+  {
+    "revision": "ef5f09fb6c30a77e7021",
+    "url": "/shopify_challenge_shoppies/static/css/main.5c3b62d3.chunk.css"
+  },
+  {
+    "revision": "6ae2d5188e19cafedbbb",
+    "url": "/shopify_challenge_shoppies/static/js/2.671db225.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopify_challenge_shoppies/static/js/2.671db225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5f09fb6c30a77e7021",
+    "url": "/shopify_challenge_shoppies/static/js/main.143ff28d.chunk.js"
+  },
+  {
+    "revision": "2e9c45f4037f42fce2da",
+    "url": "/shopify_challenge_shoppies/static/js/runtime-main.315f03de.js"
+  }
+]);
